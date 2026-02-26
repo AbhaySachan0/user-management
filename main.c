613c7a399092fbc08b03474634d0622d72165cb5
AbhaySachan0 <abhaysach05@gmail.com>
2026-02-27 01:05:27 +0530

    initialized the main

---
+++ main.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    printf( "\nWelcome to User Mangement");
+    printf("\n1. Register");
+    printf("\n2. Login");
+    printf("\n3. Exit");
+    printf("\nSelect an option.");
+    
+    int option;
+    scanf("%d",&option);
+    
+    
+    
+}
