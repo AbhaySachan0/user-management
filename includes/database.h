b45e293675435494d3693e1a1f2a0fbecd54addc
AbhaySachan0 <abhaysach05@gmail.com>
2026-03-02 22:25:23 +0530

    added database code

---
+++ includes/database.h
@@ -0,0 +1,8 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <sqlite3.h>
+
+void init_database(sqlite3 *db);
+
+#endif
---
+++ src/database.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sqlite3.h>
+#include "database.h"
+
+void init_database(sqlite3 *db) {
+    const char *sql =
+        "CREATE TABLE IF NOT EXISTS users ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "username TEXT UNIQUE NOT NULL,"
+        "password TEXT NOT NULL,"
+        "role TEXT NOT NULL,"
+        "failed_attempts INTEGER DEFAULT 0,"
+        "locked INTEGER DEFAULT 0);";
+
+    char *err_msg = 0;
+
+    if (sqlite3_exec(db, sql, 0, 0, &err_msg) != SQLITE_OK) {
+        printf("Table creation failed: %s\n", err_msg);
+        sqlite3_free(err_msg);
+    }
+}
